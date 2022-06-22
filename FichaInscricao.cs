a733519b50a3659bdb98c5703a693d04cb3235c9
Cami <camilacassimiro@gmail.com>
2022-06-22 14:31:07 -0300

    enviei ex02

--- FichaInscricao.cs
+++ FichaInscricao.cs
@@ -1,6 +1,16 @@
 namespace FichaDeInscricao;
 public class FichaInscricao
 {
+
+  public string Nome { get; set; }
+  public int Idade { get; set; }
+  public string Curso { get; set; }
+  public string Escolaridade { get; set; }
+  public DateTime DataNascimento { get; set; }
+  public double ValorCurso { get; set; }
+  public double ValorDesconto { get; set; }
+  public double ValorMulta { get; set; }
+
   public FichaInscricao(string nome, string curso, string escolaridade, DateTime dataNascimento, double valorCurso, double valorDesconto, double valorMulta)
   {
     Nome = Nome;
@@ -11,12 +21,4 @@ public class FichaInscricao
     ValorDesconto = valorDesconto;
     ValorMulta = valorMulta;
   }
-  public string Nome { get; set; }
-  public int Idade { get; set; }
-  public string Curso { get; set; }
-  public string Escolaridade { get; set; }
-  public DateTime DataNascimento { get; set; }
-  public double ValorCurso { get; set; }
-  public double ValorDesconto { get; set; }
-  public double ValorMulta { get; set; }
 }
