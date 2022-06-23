2ba179f002bbda00a0aca000bd17fd2889737651
Cami <camilacassimiro@gmail.com>
2022-06-23 14:07:09 -0300

    enviei ex 03

--- FichaInscricao.cs
+++ FichaInscricao.cs
@@ -1,24 +1,47 @@
 namespace FichaDeInscricao;
+
+//CLASSE
 public class FichaInscricao
 {
-
   public string Nome { get; set; }
   public int Idade { get; set; }
   public string Curso { get; set; }
   public string Escolaridade { get; set; }
-  public DateTime DataNascimento { get; set; }
+  public int DataNascimento { get; set; }
   public double ValorCurso { get; set; }
   public double ValorDesconto { get; set; }
   public double ValorMulta { get; set; }
 
-  public FichaInscricao(string nome, string curso, string escolaridade, DateTime dataNascimento, double valorCurso, double valorDesconto, double valorMulta)
+  //CONSTRUTOR
+  public FichaInscricao()
   {
-    Nome = Nome;
+    var dataNascimento = 0;
+
+  }
+  public FichaInscricao(string nome, string curso, string escolaridade, int dataNascimento, double valorCurso, double valorDesconto, double valorMulta)
+  {
+    Nome = nome;
     Curso = curso;
     Escolaridade = escolaridade;
     DataNascimento = dataNascimento;
     ValorCurso = valorCurso;
     ValorDesconto = valorDesconto;
     ValorMulta = valorMulta;
+  }
+
+  //Métodos
+  public void ExibirDados()
+  {
+    System.Console.WriteLine("Ano de nascimento: " + this.DataNascimento);
+    int idade = this.CalcularIdade();
+    System.Console.WriteLine("A idade é: " + idade);
+  }
+  private int CalcularIdade()
+  {
+    DateTime data = DateTime.Now;
+    int ano = data.Year;
+    int idade = ano - this.DataNascimento;
+    return idade;
+
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿namespace FichaDeInscricao;
+
+
+class Program
+{
+
+  static void Main(string[] args)
+  {
+    System.Console.WriteLine("Calculando idade");
+    FichaInscricao f = new FichaInscricao();
+    System.Console.WriteLine("Insira o ano de nascimento");
+    f.DataNascimento = Convert.ToInt32(Console.ReadLine());
+    f.ExibirDados();
+
+
+  }
+}
+
+
+
