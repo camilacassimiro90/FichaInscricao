e62d313e18fc2d9c78ea3066391952b97b682b4a
Cami <camilacassimiro@gmail.com>
2022-06-24 17:20:32 -0300

    add função

--- FichaInscricao.cs
+++ FichaInscricao.cs
@@ -7,41 +7,32 @@ public class FichaInscricao
   public int Idade { get; set; }
   public string Curso { get; set; }
   public string Escolaridade { get; set; }
-  public int DataNascimento { get; set; }
-  public double ValorCurso { get; set; }
-  public double ValorDesconto { get; set; }
-  public double ValorMulta { get; set; }
+  public DateTime DataNascimento { get; set; }
+  public decimal ValorCurso { get; set; }
+  public decimal ValorDesconto { get; set; }
+  public decimal ValorMulta { get; set; }
 
   //CONSTRUTOR
-  public FichaInscricao()
-  {
-    var dataNascimento = 0;
-
-  }
-  public FichaInscricao(string nome, string curso, string escolaridade, int dataNascimento, double valorCurso, double valorDesconto, double valorMulta)
+  public FichaInscricao(string nome, string curso, string escolaridade, DateTime dataNascimento, decimal valorCurso, decimal valorDesconto, decimal valorMulta)
   {
     Nome = nome;
     Curso = curso;
+    Idade = CalcularIdade(dataNascimento);
     Escolaridade = escolaridade;
     DataNascimento = dataNascimento;
     ValorCurso = valorCurso;
     ValorDesconto = valorDesconto;
     ValorMulta = valorMulta;
   }
-
   //Métodos
-  public void ExibirDados()
-  {
-    System.Console.WriteLine("Ano de nascimento: " + this.DataNascimento);
-    int idade = this.CalcularIdade();
-    System.Console.WriteLine("A idade é: " + idade);
-  }
-  private int CalcularIdade()
-  {
-    DateTime data = DateTime.Now;
-    int ano = data.Year;
-    int idade = ano - this.DataNascimento;
-    return idade;
 
+  private int CalcularIdade(DateTime dataNascimento)
+  {
+    var idade = 0;
+    idade = DateTime.Today.Year - dataNascimento.Year;
+    if (DateTime.Today.DayOfYear < dataNascimento.DayOfYear)
+      idade = idade - 1;
+    return idade;
   }
+
 }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,10 @@ class Program
 
   static void Main(string[] args)
   {
-    System.Console.WriteLine("Calculando idade");
-    FichaInscricao f = new FichaInscricao();
-    System.Console.WriteLine("Insira o ano de nascimento");
-    f.DataNascimento = Convert.ToInt32(Console.ReadLine());
-    f.ExibirDados();
-
-
+    // System.Console.WriteLine("Calculando idade");
+    // FichaInscricao f = new FichaInscricao("", "", "", DateTime(1990), 0, 0,0);
+    // System.Console.WriteLine("Insira o ano de nascimento");
+    // dataNascimento = Console.ReadLine("Insira o ano de nascimento");
   }
 }
 
