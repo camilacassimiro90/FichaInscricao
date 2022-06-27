ce4e1cc72a2c226c3e45cdcab7da2fc0aef4f71a
Cami <camilacassimiro@gmail.com>
2022-06-27 17:26:23 -0300

    enviei ex 07

--- CobrarCurso.cs
+++ CobrarCurso.cs
@@ -6,10 +6,26 @@ public class CobrarCurso
   private decimal ValorMulta;
   private decimal ValorDesconto;
 
+  //CONSTRUTOR
   public CobrarCurso(decimal valorCurso, decimal valorMulta, decimal valorDesconto)
   {
     this.ValorCurso = valorCurso;
     this.ValorMulta = valorMulta;
     this.ValorDesconto = valorDesconto;
   }
+
+  //MÉTODO
+  private decimal Calculo(decimal valorCurso, decimal valorMulta, decimal valorDesconto)
+  {
+    if (this.ValorMulta > 0)
+    {
+      return this.ValorCurso + this.ValorMulta;
+    }
+    else
+    {
+      return this.ValorCurso - this.ValorDesconto;
+    }
+
+  }
+
 }
